45c3054c97bf729f9bd2d7cea38ea0b997910a64
Divjyot Singh <singhdivjyot911@gmail.com>
2020-09-20 04:32:25 +0530

    Done with the basket update on click of add to basket

--- src/Product.js
+++ src/Product.js
@@ -1,6 +1,23 @@
 import React from "react";
 import "./Product.css";
+import { useStateValue } from "./StateProvider";
+
 function Product({id, title,image,price,rating}) {
+  const [{basket}, dispatch]= useStateValue();
+  const addToBasket = () =>{
+    //dispatch the item to the data layer
+    dispatch({
+      type: 'ADD_TO_BASKET',
+      item:{
+        id:id,
+        title:title,
+        image:image,
+        price:price,
+        rating:rating
+      }
+    })
+
+  }
   return (
     <div className="product">
       <div className="product_info">
@@ -20,7 +37,7 @@ function Product({id, title,image,price,rating}) {
         src={image}
         alt="product"
       />
-      <button>Add to Cart</button>
+      <button onClick={addToBasket}>Add to Cart</button>
     </div>
   );
 }
