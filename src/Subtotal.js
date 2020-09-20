4e56885929bc7a25dd47c8fad26aed4324618121
Divjyot Singh <singhdivjyot911@gmail.com>
2020-09-21 02:55:55 +0530

    done with subtotal

--- src/Subtotal.js
+++ src/Subtotal.js
@@ -1,29 +1,37 @@
 import React from "react";
 import "./Subtotal.css";
 import CurrencyFormat from "react-currency-format";
+import { useStateValue } from "./StateProvider";
 function Subtotal() {
-  return <div className="subtotal">
+  const [{ basket }] = useStateValue();
+  //calculate basket total
+  const calculateTotal = (basket) => {
+    return basket.reduce((price, item) => item.price + price, 0);
+  };
+  return (
+    <div className="subtotal">
       <CurrencyFormat
-          renderText={(value)=>(
-             <>
-                <p>
-                    {/* Part of homework */}
-                    Subtotal(0 items):
-                    <strong>0</strong>
-                </p> 
-                <small className="subtotal_gift">
-                    <input type="checkbox" /> This order contains gift
-                </small>
-            </>
-          )}
-          decimalScale={2}
-          value={0} // part of homework
-          displayType={"text"}
-          thousandSeperator={true}
-          prefix={"$"}
+        renderText={(value) => (
+          <>
+            <p>
+              {/* Part of homework */}
+              Subtotal({basket.length} items):{" "}
+              <strong>${calculateTotal(basket)}</strong>
+            </p>
+            <small className="subtotal_gift">
+              <input type="checkbox" /> This order contains gift
+            </small>
+          </>
+        )}
+        decimalScale={2}
+        value={0} // part of homework
+        displayType={"text"}
+        thousandSeperator={true}
+        prefix={"$"}
       />
       <button>Proceed to Checkout</button>
-  </div>;
+    </div>
+  );
 }
 
 export default Subtotal;
